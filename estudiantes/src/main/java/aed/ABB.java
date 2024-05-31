ac9c5a89e6051b2adbc20c020ffbccaf2dcc5acb
NadiaBarco <48421521+NadiaBarco@users.noreply.github.com>
2024-05-31 14:23:47 -0300

    Update ABB.java

--- estudiantes/src/main/java/aed/ABB.java
+++ estudiantes/src/main/java/aed/ABB.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     // Agregar atributos privados del Conjunto
     private Nodo raiz;
-    private int size;
+    private int size=0;
     private Conjunto elems; 
     private class Nodo {
         // Agregar atributos privados del Nodo
@@ -29,30 +29,23 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     public ABB() {
         Nodo raiz=null;
     }
-    public int longitud_rama(Nodo n){
-        Nodo actual=n;
-        int k=0;
-        if (actual!=null){
-            k++;
-            longitud_rama(actual.der);
-            
-        }
-        if (actual.der ==null){
+    public int longitud_rama(Nodo n){//raiz =4, n=5 
+        Nodo actual=n;//4,5
+
+        if (actual!=null){ //4!=null ,5!=null
+            contador();//k=1 ,k=2
+            longitud_rama(actual.der);//5
             longitud_rama(actual.izq);
         }
-        while (actual!=raiz){
-            longitud_rama(actual.up);
-        }
-
-        return k;
+        return size;
+    }
+    public void contador(){
+        size++;
     }
     public int cardinal() {
-        int total=0;
-        if (raiz==null){
-            return 0;
-        }else {
-            total=longitud_rama(raiz.der)+longitud_rama(raiz.izq)+1;
-        }
+        int total;
+        total=longitud_rama(raiz);
+        
         return total;
     }
 
@@ -62,8 +55,8 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
     public T minimoNodo(Nodo n){
         Nodo minimo=n;
         Nodo actual=n;
-        if (actual!=null){
-            minimo=actual;
+        if (actual.izq!=null){
+            minimo=actual.izq;
             minimoNodo(actual.izq);
         }
         return minimo.valor;
@@ -71,12 +64,12 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
 
     public T maximoNdo(Nodo n){
         Nodo maximo=n;
-        Nodo actual;
-        if (maximo!=null){
-            actual=maximo;
+        Nodo actual=n;
+        if (maximo.der!=null){
+            actual=maximo.der;
             maximoNdo(maximo.der);
         }
-        return maximo.valor;
+        return actual.valor;
     }
 
     public T maximo(){
@@ -84,41 +77,85 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
         return maximoNdo(raiz);
     }
 
-    public void insertarRec(Nodo n, Nodo m){
-        if (n==null){
-            n=m;
-        }
-        if (n.valor.compareTo(m.valor)==0){
-            return;
-        }
-         if(n.valor.compareTo(m.valor)>0){
-            insertarRec(n.izq, m);
-            
-        if (n.valor.compareTo(m.valor)<=0){
-            insertarRec(n.der,m);
-            if (n.der==null){
-                n.der=m;
-            }
-        }
-        }
-    }
-
     public void insertar(T elem){
         Nodo nuevo=new Nodo(elem);
         if (raiz==null){
             raiz=nuevo;
             return;
+        }else{
+            insertarNodo(raiz,elem);
         }
-        insertarRec(raiz,nuevo);
-        
+      
+    }
+
+    public void insertarNodo(Nodo n, T elem){
+        Nodo nuevo=new Nodo(elem);
+        if (n==null){
+            n=nuevo;
+        }else 
+        {
+            if (n.valor.compareTo(elem)==0){
+                return;
+            }
+            if (n.valor.compareTo(elem)>0 && n.izq==null){
+                n.izq=nuevo;
+
+
+            }else{
+                insertarNodo(n.der,elem);
+            }
+            if (n.valor.compareTo(elem)<0 && n.der==null) {
+                n.der=nuevo;
+            }else{
+                insertarNodo(n.izq, elem);
+            }
+        }
     }
 
     public boolean pertenece(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        return perteneceAlNodo(raiz,elem);
+    }
+
+    public boolean perteneceAlNodo(Nodo n,T elem){
+        int comparacion=n.valor.compareTo(elem);//n=5,elem =2--n=1 , elem =2
+        if (comparacion==0){
+            return true;
+        }else{
+            if (comparacion>0&&n.izq!=null){//5>2
+                perteneceAlNodo(n.izq, elem);//perteneceN(1,2)
+            }else if (comparacion<0&&n.der!=null){//1<2
+                perteneceAlNodo(n.der, elem);//n==2 ,elem=2
+            }
+        }
+        return false;
     }
 
     public void eliminar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        eliminarNodo(raiz,elem);
+    }
+    public void eliminarNodo(Nodo n,T elem){//raiz=3 elem=5
+        int comparacion=(n.valor).compareTo(elem);//3!=5
+        if(comparacion==0 && n.der==null &&n.izq==null){
+            n=null;
+        }else if(comparacion==0 && (n.der!=null && n.izq!=null)){
+            n=n.izq;
+        }
+        else{
+            if(comparacion>0){
+                eliminarNodo(n.der, elem);
+            }else if(comparacion==0 && n.der!=null){
+
+            }else if(comparacion<0){
+                eliminarNodo(n.izq, elem);
+            }
+        }
+    }
+
+    public Nodo obtenerPredecesor(Nodo n){
+        if (n.izq!=null&&(n.izq.izq!=null|| n.izq.der!=null)){
+
+        }
+        return raiz;
     }
 
     public String toString(){
@@ -143,9 +180,16 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
 
     public static void main(String[] args) {
         
-        ABB  arAbb= new ABB<>();
-
+        ABB<Integer>  arAbb= new ABB<Integer>();
+        arAbb.insertar(2);
+        arAbb.insertar(1);
+        arAbb.insertar(3);
+        arAbb.insertar(4);
+ 
+        System.out.println(arAbb.cardinal());
+        //System.out.println(arAbb.maximo());
+        //System.out.println(arAbb.raiz.valor);
+        //System.out.println(arAbb.maximo());
     }
 
 }
-
